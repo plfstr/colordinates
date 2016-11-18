e3811a3e6f36c24e08ff821bd1a16568764c7e5f
Paul Foster <paulfosterdesign@hotmail.com>
2016-11-18 16:02:22 +0000

    Service Worker Added

---
+++ sw.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'dependencies-cache',
+	REQUIRED_FILES = [
+	  '/',
+	  'index.html',
+	  'app.min.js',
+	  'styles.css'
+	];
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(REQUIRED_FILES);
+      })
+      .then(function() {
+        return self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+	      if (response) {
+	      return response;
+    	}
+    	return fetch(event.request);
+      }
+    )
+  );
+});
+	
+self.addEventListener('activate', function(event) {
+	event.waitUntil(self.clients.claim());
+});
