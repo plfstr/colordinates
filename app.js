dbdc7e1fae178e94fd2c4f2eca312615d088bd35
Paul Foster <paulfosterdesign@outlook.com>
2019-05-22 00:21:04 +0100

    Removed WinRT APIs

--- app.js
+++ app.js
@@ -90,46 +90,7 @@ if (navigator.geolocation) {
     // Display Colour Value
     domColorvalue.textContent = domOutputcolour;
     domOutput.style.backgroundColor = domOutputcolour;
-    domOutput.appendChild(domColorvalue);
-    
-    if (typeof Windows !== 'undefined'&&
-        typeof Windows.UI !== 'undefined' &&
-        typeof Windows.UI.Notifications !== 'undefined') {
-	    // Windows Hosted Web App! Your code goes inside this condition
-	    
-	    // Build tile content
-		var tileContent = new Windows.Data.Xml.Dom.XmlDocument();
-		
-		var tile = tileContent.createElement("tile");
-		tileContent.appendChild(tile);
-		
-			var visual = tileContent.createElement("visual");
-			visual.setAttribute("branding","nameAndLogo")
-			tile.appendChild(visual);
-			
-				var bindingMedium = tileContent.createElement("binding");
-				bindingMedium.setAttribute("template", "TileMedium");
-				visual.appendChild(bindingMedium);
-				
-					var tileColorval = tileContent.createElement("text");
-					tileColorval.setAttribute("hint-style", "base");
-					tileColorval.setAttribute("hint-wrap", "true");
-					tileColorval.innerText = domOutputcolour;
-					bindingMedium.appendChild(tileColorval);
-					
-					var tileGeoval = tileContent.createElement("text");
-					tileGeoval.setAttribute("hint-style", "captionSubtle");
-					tileGeoval.setAttribute("hint-wrap", "true");
-					tileGeoval.innerText = position.coords.latitude + ',' + position.coords.longitude;
-					bindingMedium.appendChild(tileGeoval);
-	    
-		// Update tile
-		var notifications = Windows.UI.Notifications;
-		var tileNotification = new notifications.TileNotification(tileContent);
-		
-		notifications.TileUpdateManager.createTileUpdaterForApplication().update(tileNotification);
-   	}
-    
+    domOutput.appendChild(domColorvalue);   
     domOutput.focus();
   }
 
