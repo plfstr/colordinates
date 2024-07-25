175623997a3a21874164c1950c85a356778a9566
Paul <plfstr@users.noreply.github.com>
2024-07-25 11:13:55 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ var CACHE_NAME = 'dependencies-cache',
 	REQUIRED_FILES = [
 	  'index.html',
 	  'app.min.js?20220805',
-	  'styles.css?20211129'
+	  'styles.css?20240725'
 	];
 
 self.addEventListener('install', function(event) {
