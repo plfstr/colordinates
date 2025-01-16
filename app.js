504cb0a7ed26379e5f4a9f46913a74ea7732987e
Paul <plfstr@users.noreply.github.com>
2025-01-16 13:17:16 +0000

    Set BG color via dataset value

--- app.js
+++ app.js
@@ -84,14 +84,14 @@ if (navigator.geolocation) {
   }
 
   function makeColor(position) {
-
     userFeedback();
-
     var domOutputcolour = 'HSLA(' + makeHue(position) + ', ' + makeSat(position) + '%, 50%, 1)';
-
-    // Display Colour Value
-    domColorvalue.textContent = domOutputcolour;  
-    domOutput.style.backgroundColor = domOutputcolour;
+    domColorvalue.textContent = domOutputcolour;    
+    if (CSS.supports("background-color", "attr(data-geocolor type(color)")) {
+      domOutput.dataset.geocolor = domOutputcolour;
+    } else {
+      domOutput.style.backgroundColor = domOutputcolour;
+    }
   }
 
   domButton.addEventListener('click', fetchGeo, false);
